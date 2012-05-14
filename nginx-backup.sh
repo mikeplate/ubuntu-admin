a304b0f39309d01020fa80aed920c610e9810afb
Mikael Plate <contact@mikeplate.com>
2012-05-14 21:35:09 +0200

    Backup script for Nginx site directories

--- nginx-backup.sh
+++ nginx-backup.sh
@@ -2,7 +2,51 @@
 #
 # Backup all sites and the configuration of nginx to /srv/www/backup directory
 
-# UNDER DEVELOPMENT. JUST TESTING.
+function do_backup {
+    local DIR=$1
+    local NAME=$2
 
-find . -type f -exec stat -c "%y" {} \; | sort -n | tail -1
+    local LAST_MOD=$(find $DIR -type f -exec stat -c '%y' {} \; | sort -n | tail -1)
+    local BACKUP_FILE=/var/backups/nginx/$NAME.tar.gz
+    if [ -f $BACKUP_FILE ]; then
+        local BACKUP_MOD=$(stat -c '%y' $BACKUP_FILE)
+    else
+        local BACKUP_MOD=''
+    fi
+
+    if [[ "$LAST_MOD" > "$BACKUP_MOD" ]]; then
+        echo "Create new backup for $NAME"
+        tar -c $DIR | gzip > $BACKUP_FILE
+        chown root:root $BACKUP_FILE
+        chmod 700 $BACKUP_FILE
+    else
+        echo "Skipping backup for $NAME"
+    fi
+}
+
+# Ensure backup location exists
+if [ ! -d /var/backups/nginx ]; then
+    mkdir -p /var/backups/nginx
+    chown root:root /var/backups/nginx
+    chmod 700 /var/backups/nginx
+fi
+
+# Backup all nginx directories
+for BASE_DIR in /srv/www/*; do
+    if [ -d $BASE_DIR ]; then
+        BASE_NAME=$(basename $BASE_DIR)
+        if [ -d $BASE_DIR/public ]; then
+            do_backup $BASE_DIR $BASE_NAME
+        else
+            for SUB_DIR in $BASE_DIR/*; do
+                if [ -d $SUB_DIR ]; then
+                    SUB_NAME=$(basename $SUB_DIR)
+                    if [ ! "$SUB_NAME" == 'dev' ]; then
+                        do_backup $SUB_DIR "$BASE_NAME-$SUB_NAME"
+                    fi
+                fi
+            done
+        fi
+    fi
+done
 
